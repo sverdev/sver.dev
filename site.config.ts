dd0942c49654ae7b25e0a0d9c6641c817790f688
sh4lluloveme <wh3n_gg@kakao.com>
2023-10-20 14:16:09 +0900

    edit: change notion pageid

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'f7414cf10bfd4a27a14ff28acfa91b0c',
+  rootNotionPageId: '939d3eeb835547e6b293573bdb58f2fd',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
