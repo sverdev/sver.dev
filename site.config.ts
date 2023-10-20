054a6ff57ebdc96c2aacf626dd9a30b9586016cd
sh4lluloveme <wh3n_gg@kakao.com>
2023-10-20 14:08:58 +0900

    edit: disable github button

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: '#',
-  github: 'sverdev',
+  //github: 'sverdev',
   // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
